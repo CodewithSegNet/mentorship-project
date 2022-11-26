8663a1ed48f4a4574687c8e5b05b1a94794873df
Olusegun Emmanuel <91101596+CodewithSegNet@users.noreply.github.com>
2022-11-26 01:15:00 +0100

    adding new files

---
+++ index.js
@@ -0,0 +1,21 @@
+// jshint esversion:6
+const navSlide = () => {
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.links');
+const navlinks = document.querySelectorAll('.links li');
+
+burger.addEventListener('click', () => {
+nav.classList.toggle('nav-active');
+
+navlinks.forEach((link, index) => {
+if (link.style.animation) {
+  link.style.animation = '';
+} else {
+  link.style.animation = 'navLinkFade 0.5s ease forwards';
+}
+  });
+  burger.classList.toggle('toggle');
+});
+}
+navSlide();
