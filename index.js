56f456ff674e23f50e1ef79a8912667ba0ce7268
Olusegun Emmanuel <91101596+CodewithSegNet@users.noreply.github.com>
2023-01-08 22:40:02 +0100

    added files

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 // jshint esversion:6
 const navSlide = () => {
-
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.links');
-const navlinks = document.querySelectorAll('.links li');
+
+
 
 burger.addEventListener('click', () => {
 nav.classList.toggle('nav-active');
-
-navlinks.forEach((link, index) => {
-if (link.style.animation) {
-  link.style.animation = '';
-} else {
-  link.style.animation = 'navLinkFade 0.5s ease forwards';
-}
-  });
-  burger.classList.toggle('toggle');
+burger.classList.toggle('toggle');
 });
+
+
 }
 navSlide();
+
+const arrow1 = document.getElementById('down-logo2');
+const arrow = document.getElementById('down-logo1');
+const submenu = document.getElementById('sub');
+const submenu1 = document.getElementById('sub1');
+const links = document.querySelector('.submenu1');
+const links1 = document.querySelector('.submenu');
+
+
+links.addEventListener('click', () => {
+  submenu.classList.toggle('sub-active');
+  arrow.classList.toggle('arrows');
+});
+
+links1.addEventListener('click', () => {
+  submenu1.classList.toggle('sub-active');
+  arrow1.classList.toggle('arrows');
+});
